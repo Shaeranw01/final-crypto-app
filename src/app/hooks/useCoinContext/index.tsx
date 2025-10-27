38631ad657d71d806783346f16c6f2563ac5c41a
Shaeranw01 <sharan.w01@gmail.com>
2025-10-27 07:14:07 -0700

    context type fixed

--- src/app/hooks/useCoinContext/index.tsx
+++ src/app/hooks/useCoinContext/index.tsx
@@ -1,7 +1,8 @@
 import { CoinDataContext } from "@/app/context/coinDataContext";
 import { useContext } from "react";
+import { CoinContextType } from "@/interfaces/CoinContextType";
 
-export function useCoinContext() {
+export function useCoinContext(): CoinContextType {
   const context = useContext(CoinDataContext);
   if (!context) {
     throw new Error("useCoinContext must be used within a CoinDataProvider");
